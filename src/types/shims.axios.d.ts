fbcdecd89a909e4f1db45b2ec6557b0962543f4a
杨柠滔 <372728339@qq.com>
2022-05-17 16:47:24 +0800

    feat: 扩展axios类型

---
+++ src/types/shims.axios.d.ts
@@ -0,0 +1,11 @@
+import "axios";
+declare module "axios" {
+  interface AxiosRequestConfig {
+    // 请求地址，不带 baseURL
+    url: string;
+    // 反回值是否没有 code 属性
+    dataNotIncludeCode?: boolean;
+    // 不要携带 token
+    notNeedToken?: boolean;
+  }
+}
