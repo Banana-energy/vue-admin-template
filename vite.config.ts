a67a00222aa477511332e796f90eeedb8f1f5b29
杨柠滔 <372728339@qq.com>
2022-11-17 16:11:50 +0800

    chore: 优化依赖预构建

--- vite.config.ts
+++ vite.config.ts
@@ -9,34 +9,54 @@ import Icons from "unplugin-icons/vite";
 import IconsResolver from "unplugin-icons/resolver";
 
 // https://vitejs.dev/config/
-export default defineConfig(() => ({
-  server: {
-    host: "0.0.0.0",
-  },
-  plugins: [
-    AutoImport({
-      dts: "src/types/auto-import.d.ts",
-      imports: ["vue", "vue-router", "pinia"],
-      resolvers: [ElementPlusResolver()],
-      eslintrc: {
-        enabled: false, // 默认false, true启用。生成一次就可以，避免每次工程启动都生成
-        filepath: "./.eslintrc-auto-import.json", // 生成json文件
-        globalsPropValue: true,
-      },
-    }),
-    Components({
-      dts: "src/types/components.d.ts",
-      resolvers: [ElementPlusResolver(), IconsResolver()],
-    }),
-    Vue(),
-    VueSetupExtend(),
-    Icons({
-      autoInstall: true,
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+export default defineConfig(({ mode }) => {
+  const isProd = mode === "production";
+  return {
+    server: {
+      host: "0.0.0.0",
     },
-  },
-}));
+    build: {
+      target: "esnext",
+      sourcemap: !isProd,
+    },
+    plugins: [
+      AutoImport({
+        dts: "src/types/auto-import.d.ts",
+        imports: ["vue", "vue-router", "pinia"],
+        resolvers: [ElementPlusResolver()],
+        eslintrc: {
+          enabled: false, // 默认false, true启用。生成一次就可以，避免每次工程启动都生成
+          filepath: "./.eslintrc-auto-import.json", // 生成json文件
+          globalsPropValue: true,
+        },
+      }),
+      Components({
+        dts: "src/types/components.d.ts",
+        resolvers: [ElementPlusResolver(), IconsResolver()],
+      }),
+      Vue(),
+      VueSetupExtend(),
+      Icons({
+        autoInstall: true,
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        target: "esnext",
+      },
+      include: [
+        "vue",
+        "vue-router",
+        "pinia",
+        "element-plus/es/locale/lang/zh-cn",
+        "element-plus/es/locale/lang/en",
+        "kiwi-intl",
+      ],
+    },
+  };
+});
