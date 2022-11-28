77f3f454111fb38d19d7d99f5659caf7a03aabe3
杨柠滔 <372728339@qq.com>
2022-11-28 13:48:57 +0800

    chore: 忽略自动导入类型文件

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -21,6 +21,7 @@
   </div>
 </template>
 <script setup lang="ts" name="Logo">
+import config from "@/config/env.config";
 interface Props {
   isCollapse: boolean;
 }
@@ -28,7 +29,7 @@ interface Props {
 withDefaults(defineProps<Props>(), {
   isCollapse: false,
 });
-const title = ref<string>("Vue-Admin-Template");
+const title = config.appTitle;
 const logo = ref<string>(
   "https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png"
 );
