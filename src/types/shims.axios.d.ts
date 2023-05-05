4382ecf1e6b7f97b72ff78e9bf61f6ea0572e362
Banana Energy <372728339@qq.com>
2023-05-05 20:13:18 +0800

    chore: 升级依赖

--- src/types/shims.axios.d.ts
+++ src/types/shims.axios.d.ts
@@ -9,4 +9,8 @@ declare module "axios" {
     // 不要携带 token
     notNeedToken?: boolean;
   }
+
+  interface AxiosInterceptorManager<V> {
+    use(onFulfilled?: ((value: V) => void) | null, onRejected?: ((error: any) => any) | null, options?: AxiosInterceptorOptions): number;
+  }
 }
